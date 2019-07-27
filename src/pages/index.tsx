58c7083db6663db674a591ca9cfad1c816b1e4c0
Niputi <7137178+Niputi@users.noreply.github.com>
2019-07-27 22:51:35 +0200

    removed foter/navbar compoents as it was a bit funky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,17 @@ import NavbarComponent from "../components/navbar"
 import { Router } from "@reach/router"
 import Home from "./home"
 import Donate from "./donate"
+import About from "./about"
 import { FooterComponent } from "../components/footer";
 
 export default class Root extends React.Component {
   render() {
     return (
-      <div>
-        <NavbarComponent />
-        <Router>
-          <Home path="/" />
-          <Donate path="/donate"></Donate>
-        </Router>
-        <FooterComponent />
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-j8y0ITrvFafF4EkV1mPW0BKm6dp3c+J9Fky22Man50Ofxo2wNe5pT1oZejDH9/Dt" crossOrigin="anonymous" />
-      </div>
+      <Router>
+        <Home path="/" />
+        <Donate path="/donate"></Donate>
+        <About path="/about"></About>
+      </Router>
     )
   }
 }
