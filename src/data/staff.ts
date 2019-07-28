b4f569ac87d9ceacd32e70bb431779ad3b2656ef
Niputi <7137178+Niputi@users.noreply.github.com>
2019-07-28 22:02:22 +0200

    move about data to seperate files

---
+++ src/data/staff.ts
@@ -0,0 +1,27 @@
+export default [
+  {
+    name: "xRappy",
+    desc: "Server Admin",
+    uid: "102405515060994048",
+  },
+  {
+    name: "IA",
+    desc: "Server Admin",
+    uid: "147447799573774336",
+  },
+  {
+    name: "Tal",
+    desc: "Server Mod",
+    uid: "166157397738520578",
+  },
+  {
+    name: "Meagan",
+    desc: "Server Mod",
+    uid: "189158295704043521",
+  },
+  {
+    name: "Homer12346",
+    desc: "Server Maid",
+    uid: "136823052368347136",
+  },
+]
